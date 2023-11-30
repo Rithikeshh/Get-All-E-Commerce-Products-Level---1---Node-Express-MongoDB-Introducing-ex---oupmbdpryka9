afc8ea922ce0016709ea89decdf9b11f9d3bc204
Rithikeshh <rithikeshh@gmail.com>
2023-11-30 17:44:49 +0530

    updated commit

--- src/app.js
+++ src/app.js
@@ -10,14 +10,21 @@ app.use(express.json());
 
 // Write GET endpoint for sending all the products to client here
 // Endpoint - /api/v1/products
-app.get("/api/v1/:product", (req, res) => {
-    const {product} = req.params
-    if(product == 'products'){
-        res.status(200).json({"status": "success", message: "Products fetched successfully", data: {"product":{...products}}});
-        
-    }else{
-        res.status(404).json({"message": "Product not found"})
-    }
+app.get("/api/v1/products", (req, res) => {
+    if (products.length === 0) {
+        return res.status(404).json({
+          status: "failed",
+          message: "Product not found",
+        });
+      }
+    
+      res.status(200).json({
+        status: "success",
+        message: "Product fetched successfully",
+        data: {
+          products: products,
+        },
+      });
 })
 
 module.exports = app;
